18116a375051a53d6b0528a25095e65083812ab5
Sergei <sergei.melnik90@gmail.com>
2018-03-15 14:33:31 +0100

    Auto stash before merge of "master" and "origin/master"

--- js/board.js
+++ js/board.js
@@ -1,36 +1,44 @@
 
 class Board {  
 
-  constructor(){
-    this.data = [
-      [0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0]
-    ];
-  }
-
-  makeMove(col, player){
-    // col 0-6
-
-    // return true if move possible, false otherwise (column full)
-  }
-
-  render(){
-    // in the div with the class "board" render all rows and columns from the data array
-    let html = '';
-    for(let row of this.data){
-      console.log(row);
-      html += 'start row';
-      for(let col of row){
-        html += 'one slot'; // different css classes depending on slot 0, 1 or 2
-      }
-      html += 'end row';
+    constructor(){
+      this.data = [
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0]
+      ];
     }
-    $('.board').html(html);
+
+    makeMove(col, player){
+      // col 0-6
+      if (col == 0) {
+        return true
+      } else {
+        false
+      }
+      // return true if move possible, false otherwise (column full)
+    }
+
   }
+    render(){
+      // in the div with the class "board" render all rows and columns from the data array
+      let html = '';
+      for(let row of data){
+        html += 'start row';
+        for(let col of row){
+          html += 'one slot'; // different css classes depending on slot 0, 1 or 2
+        }
+        html += 'end row';
+      }
+      $('.board').html(html)
+    }
 
+  
+
+
+  }
+  
 }
-
--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(start);
 function start() {
   
+  let c=document.getElementById("myCanvas");
+	let ctx=c.getContext("2d");
+	ctx.beginPath();
+	ctx.arc(75,75,50,0,2*Math.PI);
+	ctx.stroke();
+
 }
