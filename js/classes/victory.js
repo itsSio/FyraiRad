74c62f17188a813c535f3d90567f94707dfa85a4
Unknown <jessica_nurmi@hotmail.com>
2018-03-15 22:17:39 +0100

    Lagt till klassen Victory
    Anropas med numret på spelaren som vann (1 eller 2).
    Se klassen victorydemo.html för exempel på hur klassen anropas.

---
+++ js/classes/victory.js
@@ -0,0 +1,27 @@
+class Victory {
+
+    constructor(players) { //i konstruktorn så tar vi in alla beroenden som vi har till resten av spelet: Vi behöver spelarna för att kunna förtydliga förlorarens nederlag, och fira vinnaren.
+        this.players = players;  //Den här klassen (refereras via "this") har nu en variabel som heter "players",
+    }
+
+
+    victoryToPlayer(playerNumber) {
+        let playerThatWon;
+        let playerThatSucked;
+        if (playerNumber === 1) {
+            playerThatWon = players[0]; //array nollindexerad, så spelare 1 är på plats 0.
+            playerThatSucked = players[1];
+        } else {
+            playerThatWon = players[1];
+            playerThatSucked = players[0];
+        }
+
+        $('body').text('');
+        $('body').css('background-color', playerThatWon.color);
+        $('body').append('<h1>VINNARE: '+ playerThatWon.name +'</h1>');
+
+        $('body').append('<h1>FÖLJANDE SPELARE SÖG: '+ playerThatSucked.name +'</h1>');
+
+
+    }
+}
